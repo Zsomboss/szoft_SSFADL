30f7b78faeb71a4d5acc0ce936b71aa5f2310ec3
User <User@DESKTOP-OJANKBF>
2024-04-17 23:43:08 +0200

    gyak

---
+++ mintazh2/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace mintazh2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            szamDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            kerdesDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            v1DataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            v2DataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            v3DataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            kepDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            helyesValaszDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            vizsgaKerdesekBindingSource = new BindingSource(components);
+            buttonOpen = new Button();
+            buttonSave = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            buttonDelete = new Button();
+            buttonAddNew = new Button();
+            buttonEdit = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)vizsgaKerdesekBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { szamDataGridViewTextBoxColumn, kerdesDataGridViewTextBoxColumn, v1DataGridViewTextBoxColumn, v2DataGridViewTextBoxColumn, v3DataGridViewTextBoxColumn, kepDataGridViewTextBoxColumn, helyesValaszDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = vizsgaKerdesekBindingSource;
+            dataGridView1.Location = new Point(53, 72);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(428, 306);
+            dataGridView1.TabIndex = 0;
+            // 
+            // szamDataGridViewTextBoxColumn
+            // 
+            szamDataGridViewTextBoxColumn.DataPropertyName = "Szam";
+            szamDataGridViewTextBoxColumn.HeaderText = "Szam";
+            szamDataGridViewTextBoxColumn.Name = "szamDataGridViewTextBoxColumn";
+            // 
+            // kerdesDataGridViewTextBoxColumn
+            // 
+            kerdesDataGridViewTextBoxColumn.DataPropertyName = "Kerdes";
+            kerdesDataGridViewTextBoxColumn.HeaderText = "Kerdes";
+            kerdesDataGridViewTextBoxColumn.Name = "kerdesDataGridViewTextBoxColumn";
+            // 
+            // v1DataGridViewTextBoxColumn
+            // 
+            v1DataGridViewTextBoxColumn.DataPropertyName = "V1";
+            v1DataGridViewTextBoxColumn.HeaderText = "V1";
+            v1DataGridViewTextBoxColumn.Name = "v1DataGridViewTextBoxColumn";
+            // 
+            // v2DataGridViewTextBoxColumn
+            // 
+            v2DataGridViewTextBoxColumn.DataPropertyName = "V2";
+            v2DataGridViewTextBoxColumn.HeaderText = "V2";
+            v2DataGridViewTextBoxColumn.Name = "v2DataGridViewTextBoxColumn";
+            // 
+            // v3DataGridViewTextBoxColumn
+            // 
+            v3DataGridViewTextBoxColumn.DataPropertyName = "V3";
+            v3DataGridViewTextBoxColumn.HeaderText = "V3";
+            v3DataGridViewTextBoxColumn.Name = "v3DataGridViewTextBoxColumn";
+            // 
+            // kepDataGridViewTextBoxColumn
+            // 
+            kepDataGridViewTextBoxColumn.DataPropertyName = "Kep";
+            kepDataGridViewTextBoxColumn.HeaderText = "Kep";
+            kepDataGridViewTextBoxColumn.Name = "kepDataGridViewTextBoxColumn";
+            // 
+            // helyesValaszDataGridViewTextBoxColumn
+            // 
+            helyesValaszDataGridViewTextBoxColumn.DataPropertyName = "HelyesValasz";
+            helyesValaszDataGridViewTextBoxColumn.HeaderText = "HelyesValasz";
+            helyesValaszDataGridViewTextBoxColumn.Name = "helyesValaszDataGridViewTextBoxColumn";
+            // 
+            // vizsgaKerdesekBindingSource
+            // 
+            vizsgaKerdesekBindingSource.DataSource = typeof(VizsgaKerdesek);
+            // 
+            // buttonOpen
+            // 
+            buttonOpen.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonOpen.Location = new Point(729, 42);
+            buttonOpen.Name = "buttonOpen";
+            buttonOpen.Size = new Size(75, 23);
+            buttonOpen.TabIndex = 1;
+            buttonOpen.Text = "Megnyitás";
+            buttonOpen.UseVisualStyleBackColor = true;
+            buttonOpen.Click += buttonOpen_Click;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonSave.Location = new Point(629, 42);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(85, 23);
+            buttonSave.TabIndex = 2;
+            buttonSave.Text = "Mentés";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.DataBindings.Add(new Binding("Text", vizsgaKerdesekBindingSource, "Kerdes", true));
+            textBox1.Location = new Point(520, 123);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(297, 32);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.DataBindings.Add(new Binding("Text", vizsgaKerdesekBindingSource, "V2", true));
+            textBox2.Location = new Point(520, 199);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(297, 32);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.DataBindings.Add(new Binding("Text", vizsgaKerdesekBindingSource, "V1", true));
+            textBox3.Location = new Point(520, 161);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(297, 32);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox4.DataBindings.Add(new Binding("Text", vizsgaKerdesekBindingSource, "V3", true));
+            textBox4.Location = new Point(520, 237);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(297, 32);
+            textBox4.TabIndex = 6;
+            // 
+            // buttonDelete
+            // 
+            buttonDelete.Location = new Point(325, 400);
+            buttonDelete.Name = "buttonDelete";
+            buttonDelete.Size = new Size(75, 23);
+            buttonDelete.TabIndex = 7;
+            buttonDelete.Text = "Törlés";
+            buttonDelete.UseVisualStyleBackColor = true;
+            buttonDelete.Click += buttonDelete_Click;
+            // 
+            // buttonAddNew
+            // 
+            buttonAddNew.Location = new Point(406, 400);
+            buttonAddNew.Name = "buttonAddNew";
+            buttonAddNew.Size = new Size(75, 23);
+            buttonAddNew.TabIndex = 8;
+            buttonAddNew.Text = "Új";
+            buttonAddNew.UseVisualStyleBackColor = true;
+            buttonAddNew.Click += buttonAddNew_Click;
+            // 
+            // buttonEdit
+            // 
+            buttonEdit.Location = new Point(487, 400);
+            buttonEdit.Name = "buttonEdit";
+            buttonEdit.Size = new Size(75, 23);
+            buttonEdit.TabIndex = 9;
+            buttonEdit.Text = "&Szerkesztés";
+            buttonEdit.UseVisualStyleBackColor = true;
+            buttonEdit.Click += buttonEdit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(848, 450);
+            Controls.Add(buttonEdit);
+            Controls.Add(buttonAddNew);
+            Controls.Add(buttonDelete);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(buttonSave);
+            Controls.Add(buttonOpen);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)vizsgaKerdesekBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button buttonOpen;
+        private Button buttonSave;
+        private DataGridViewTextBoxColumn szamDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn kerdesDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn v1DataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn v2DataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn v3DataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn kepDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn helyesValaszDataGridViewTextBoxColumn;
+        private BindingSource vizsgaKerdesekBindingSource;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button buttonDelete;
+        private Button buttonAddNew;
+        private Button buttonEdit;
+    }
+}
---
+++ mintazh2/FormEdit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mintazh2
+{
+    public partial class FormEdit : Form
+    {
+        public VizsgaKerdesek ÚjVizsgaKérdés = new();
+        public FormEdit()
+        {
+            InitializeComponent();
+            vizsgaKerdesekBindingSource.DataSource = ÚjVizsgaKérdés;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
